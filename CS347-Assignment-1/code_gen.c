a841d12e4e2c3790477bb6a3d8275032df3e1af2
Nitin Kedia <nitinkedia7@gmail.com>
2019-02-02 20:59:19 +0530

    Error detection and print if found using sample inputs

--- CS347-Assignment-1/code_gen.c
+++ CS347-Assignment-1/code_gen.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
 #include "name.c"
 #include "lex.c"
+#include <stdarg.h>
+
+#define MAXFIRST 16
+#define SYNCH	 SEMI
+
+int	legal_lookahead(int first_arg, ...)
+// int first_arg;
+{
+    /* Simple error detection and recovery. Arguments are a 0-terminated list of
+     * those tokens that can legitimately come next in the input. If the list is
+     * empty, the end of file must come next. Print an error message if
+     * necessary. Error recovery is performed by discarding all input symbols
+     * until one that's in the input list is found
+     *
+     * Return true if there's no error or if we recovered from the error,
+     * false if we can't recover.
+     */
+
+    va_list  	args;
+    int		tok;
+    int		lookaheads[MAXFIRST], *p = lookaheads, *current;
+    int		error_printed = 0;
+    int		rval	      = 0;
+
+    va_start( args, first_arg );
+
+    if( !first_arg )
+    {
+	if( match(EOI) )
+	    rval = 1;
+    }
+    else
+    {
+	*p++ = first_arg;
+	while( (tok = va_arg(args, int)) && p < &lookaheads[MAXFIRST] )
+	    *p++ = tok;
+
+	while( !match( SYNCH ) )
+	{
+	    for( current = lookaheads; current < p ; ++current )
+		if( match( *current ) )
+		{
+		    rval = 1;
+		    goto exit;
+		}
+
+	    if( !error_printed )
+	    {
+		fprintf( stderr, "Line %d: Syntax error\n", yylineno );
+		error_printed = 1;
+	    }
+
+	    advance();
+	}
+    }
+
+exit:
+    va_end( args );
+    return rval;
+}
+
+void terminate() {
+    remove("./output/assembly.temp");
+    remove("./output/assembly.txt");
+    remove("./output/intermediate.txt");  
+    exit(1);
+}
 
 void stmt(void);
 void stmt_list(void);
@@ -20,9 +87,9 @@ int comp_label = 0;
 void statements(void){
     /* statements -> stmt statements | stmt */
 
-    outtemp = fopen("assembly.temp", "w+");
-    outinter = fopen("intermediate.inter", "w");
-    outasm = fopen("assembly.asm", "w");
+    outtemp = fopen("./output/assembly.temp", "w+");
+    outinter = fopen("./output/intermediate.txt", "w");
+    outasm = fopen("./output/assembly.txt", "w");
     
     while(!match(EOI)){
         stmt();
@@ -45,7 +112,7 @@ void statements(void){
     fclose(outasm);
     fclose(outtemp);
     fclose(outinter);
-    remove("assembly.temp");
+    remove("./output/assembly.temp");
 }
 
 void stmt(void)
@@ -54,6 +121,10 @@ void stmt(void)
             | if expr then stmt
             | while expr do stmt
             | begin opt_stmts end */
+
+    if( !legal_lookahead( NUM_OR_ID, IF, WHILE, BEGIN, 0 ) ) {
+        terminate();
+    }
 
     char *tempvar, *tempvar2;
     if (match(NUM_OR_ID))
@@ -73,13 +144,18 @@ void stmt(void)
             strncpy(var, idname, yyleng);
             advance();
             tempvar = expr(0);
-            if(!match(SEMI)){
-                fprintf( stderr, "%d: Inserting missing semicolon\n", yylineno );
+
+            if(match(SEMI)){
+                advance();
             }
-            advance();
+            else fprintf( stderr, "%d: Inserting missing semicolon\n", yylineno );
             fprintf(outinter, "%s = %s\n", var, tempvar);
             fprintf(outtemp, "MOV %s, %s\n", var, tempvar);
             freename(tempvar);
+        }
+        else {
+            fprintf(stderr, "%d: Expected assignent operator ':='\n", yylineno);
+            terminate();
         }
     }
     else if (match(IF))
@@ -99,6 +175,7 @@ void stmt(void)
         else
         {
             fprintf(stderr, "%d: Missing then after if\n", yylineno);
+            terminate();
         }
                
     }
@@ -125,6 +202,7 @@ void stmt(void)
         else
         {
             fprintf(stderr, "%d: Missing do after while\n", yylineno);
+            terminate();
         }
     }
     else if (match(BEGIN))
@@ -137,11 +215,7 @@ void stmt(void)
 
 void stmt_list(void)
 {
-    /* stmt_list -> stmt stmt_list'
-       stmt_list' -> stmt stmt_list' |  END */
-
-    char *tempvar, *tempvar2;
-
+    /* stmt_list -> stmt stmt_list  | END */
     while (!match(END))
     {
         stmt();
@@ -347,6 +421,7 @@ char *factor()
     else
     {
         fprintf(stderr, "%d: Number or identifier expected\n", yylineno);
+        terminate();
     }
     return tempvar;
 }
