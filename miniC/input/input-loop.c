9be6749db81502c02991aa76d46618f1b0043318
Nitin Kedia <nitinkedia7@gmail.com>
2019-04-17 17:58:40 +0530

    Overhaul grammer to complete syntax analysis phase

--- miniC/input/input-loop.c
+++ miniC/input/input-loop.c
@@ -1,14 +1,16 @@
 int main(){
     int a, b;
     int area;
-    for(int i=0; i<10; i++){
+    a=10;
+    for(i=0; i<10; i++){
         area += a*b;
     }
-    float newa = a/2.0;
+    float newa= a/2.0;
     float newb = b/2.0;
     float newarea;
     int j = 10;
     while(j--){
         newarea -= newa*newb;
     }
+    for (;;) {}
 }
